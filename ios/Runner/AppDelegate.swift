74350d92d355a59d1a4bfee30a74f539d82f9107
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2022-02-02 10:40:11 +0900

    ios スプラッシュアニメーション追加

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,15 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    lazy var flutterEngine = FlutterEngine(name: "MyApp")
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // Runs the default Dart entrypoint with a default Flutter route.
+        flutterEngine.run()
+        // Used to connect plugins (only if you have plugins with iOS platform code).
+        GeneratedPluginRegistrant.register(with: self.flutterEngine)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
