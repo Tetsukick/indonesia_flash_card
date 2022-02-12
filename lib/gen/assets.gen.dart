be58c5092d4839dd06d1f316e560d3d5548d7ce2
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2022-02-12 15:29:16 +0900

    add flutter gen

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,371 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
+class $AssetsIconGen {
+  const $AssetsIconGen();
+
+  /// File path: assets/icon/app_icon.png
+  AssetGenImage get appIcon => const AssetGenImage('assets/icon/app_icon.png');
+}
+
+class $AssetsLottieGen {
+  const $AssetsLottieGen();
+
+  /// File path: assets/lottie/countdown_10s.json
+  String get countdown10s => 'assets/lottie/countdown_10s.json';
+
+  /// File path: assets/lottie/splash_screen.json
+  String get splashScreen => 'assets/lottie/splash_screen.json';
+}
+
+class $AssetsPrefectureIndonesiaGen {
+  const $AssetsPrefectureIndonesiaGen();
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Aceh.png
+  AssetGenImage get logoProvinsiAceh =>
+      const AssetGenImage('assets/prefecture_indonesia/Logo_Provinsi_Aceh.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Bali.png
+  AssetGenImage get logoProvinsiBali =>
+      const AssetGenImage('assets/prefecture_indonesia/Logo_Provinsi_Bali.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Banten.png
+  AssetGenImage get logoProvinsiBanten => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Banten.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Bengkulu.png
+  AssetGenImage get logoProvinsiBengkulu => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Bengkulu.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_DKI_Jakarta.png
+  AssetGenImage get logoProvinsiDKIJakarta => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_DKI_Jakarta.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Gorontalo.png
+  AssetGenImage get logoProvinsiGorontalo => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Gorontalo.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Jambi.png
+  AssetGenImage get logoProvinsiJambi => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Jambi.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Jawa_Barat.png
+  AssetGenImage get logoProvinsiJawaBarat => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Jawa_Barat.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Jawa_Tengah.png
+  AssetGenImage get logoProvinsiJawaTengah => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Jawa_Tengah.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Jawa_Timur.png
+  AssetGenImage get logoProvinsiJawaTimur => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Jawa_Timur.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Barat.png
+  AssetGenImage get logoProvinsiKalimantanBarat => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Barat.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Selatan.png
+  AssetGenImage get logoProvinsiKalimantanSelatan => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Selatan.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Tengah.png
+  AssetGenImage get logoProvinsiKalimantanTengah => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Tengah.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Timur.png
+  AssetGenImage get logoProvinsiKalimantanTimur => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Timur.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Utara.png
+  AssetGenImage get logoProvinsiKalimantanUtara => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Kalimantan_Utara.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Kepulauan_Bangka_Belitung.png
+  AssetGenImage get logoProvinsiKepulauanBangkaBelitung => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Kepulauan_Bangka_Belitung.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Lampung.png
+  AssetGenImage get logoProvinsiLampung => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Lampung.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Maluku.png
+  AssetGenImage get logoProvinsiMaluku => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Maluku.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Maluku_Utara.png
+  AssetGenImage get logoProvinsiMalukuUtara => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Maluku_Utara.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Nusa_Tenggara_Barat.png
+  AssetGenImage get logoProvinsiNusaTenggaraBarat => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Nusa_Tenggara_Barat.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Nusa_Tenggara_Timur.png
+  AssetGenImage get logoProvinsiNusaTenggaraTimur => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Nusa_Tenggara_Timur.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Papua.png
+  AssetGenImage get logoProvinsiPapua => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Papua.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Papua_Barat.png
+  AssetGenImage get logoProvinsiPapuaBarat => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Papua_Barat.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Riau.png
+  AssetGenImage get logoProvinsiRiau =>
+      const AssetGenImage('assets/prefecture_indonesia/Logo_Provinsi_Riau.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Sulawesi_Selatan.png
+  AssetGenImage get logoProvinsiSulawesiSelatan => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Sulawesi_Selatan.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Sulawesi_Tengah.png
+  AssetGenImage get logoProvinsiSulawesiTengah => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Sulawesi_Tengah.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Sulawesi_Tenggara.png
+  AssetGenImage get logoProvinsiSulawesiTenggara => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Sulawesi_Tenggara.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Sulawesi_Utara.png
+  AssetGenImage get logoProvinsiSulawesiUtara => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Sulawesi_Utara.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Sumatera_Selatan.png
+  AssetGenImage get logoProvinsiSumateraSelatan => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Sumatera_Selatan.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Provinsi_Yogyakarta.png
+  AssetGenImage get logoProvinsiYogyakarta => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Provinsi_Yogyakarta.png');
+
+  /// File path: assets/prefecture_indonesia/Logo_Sumatera_Barat.png
+  AssetGenImage get logoSumateraBarat => const AssetGenImage(
+      'assets/prefecture_indonesia/Logo_Sumatera_Barat.png');
+
+  /// File path: assets/prefecture_indonesia/Provinsi_Sulawesi_Barat.png
+  AssetGenImage get provinsiSulawesiBarat => const AssetGenImage(
+      'assets/prefecture_indonesia/Provinsi_Sulawesi_Barat.png');
+
+  /// File path: assets/prefecture_indonesia/logo_Provinsi_Kepulauan_Riau_Kepri.png
+  AssetGenImage get logoProvinsiKepulauanRiauKepri => const AssetGenImage(
+      'assets/prefecture_indonesia/logo_Provinsi_Kepulauan_Riau_Kepri.png');
+
+  /// File path: assets/prefecture_indonesia/logo_sumatera_utara.png
+  AssetGenImage get logoSumateraUtara => const AssetGenImage(
+      'assets/prefecture_indonesia/logo_sumatera_utara.png');
+}
+
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// File path: assets/svg/building_2.svg
+  SvgGenImage get building2 => const SvgGenImage('assets/svg/building_2.svg');
+
+  /// File path: assets/svg/buildings.svg
+  SvgGenImage get buildings => const SvgGenImage('assets/svg/buildings.svg');
+
+  /// File path: assets/svg/day.svg
+  SvgGenImage get day => const SvgGenImage('assets/svg/day.svg');
+
+  /// File path: assets/svg/eat.svg
+  SvgGenImage get eat => const SvgGenImage('assets/svg/eat.svg');
+
+  /// File path: assets/svg/event.svg
+  SvgGenImage get event => const SvgGenImage('assets/svg/event.svg');
+
+  /// File path: assets/svg/event_2.svg
+  SvgGenImage get event2 => const SvgGenImage('assets/svg/event_2.svg');
+
+  /// File path: assets/svg/event_3.svg
+  SvgGenImage get event3 => const SvgGenImage('assets/svg/event_3.svg');
+
+  /// File path: assets/svg/fashion.svg
+  SvgGenImage get fashion => const SvgGenImage('assets/svg/fashion.svg');
+
+  /// File path: assets/svg/fashion_2.svg
+  SvgGenImage get fashion2 => const SvgGenImage('assets/svg/fashion_2.svg');
+
+  /// File path: assets/svg/fashion_3.svg
+  SvgGenImage get fashion3 => const SvgGenImage('assets/svg/fashion_3.svg');
+
+  /// File path: assets/svg/fashion_4.svg
+  SvgGenImage get fashion4 => const SvgGenImage('assets/svg/fashion_4.svg');
+
+  /// File path: assets/svg/food_2.svg
+  SvgGenImage get food2 => const SvgGenImage('assets/svg/food_2.svg');
+
+  /// File path: assets/svg/islam_1.svg
+  SvgGenImage get islam1 => const SvgGenImage('assets/svg/islam_1.svg');
+
+  /// File path: assets/svg/islam_2.svg
+  SvgGenImage get islam2 => const SvgGenImage('assets/svg/islam_2.svg');
+
+  /// File path: assets/svg/money.svg
+  SvgGenImage get money => const SvgGenImage('assets/svg/money.svg');
+
+  /// File path: assets/svg/money_2.svg
+  SvgGenImage get money2 => const SvgGenImage('assets/svg/money_2.svg');
+
+  /// File path: assets/svg/shodou.svg
+  SvgGenImage get shodou => const SvgGenImage('assets/svg/shodou.svg');
+
+  /// File path: assets/svg/sport_2.svg
+  SvgGenImage get sport2 => const SvgGenImage('assets/svg/sport_2.svg');
+
+  /// File path: assets/svg/sports_1.svg
+  SvgGenImage get sports1 => const SvgGenImage('assets/svg/sports_1.svg');
+
+  /// File path: assets/svg/time.svg
+  SvgGenImage get time => const SvgGenImage('assets/svg/time.svg');
+
+  /// File path: assets/svg/time_2.svg
+  SvgGenImage get time2 => const SvgGenImage('assets/svg/time_2.svg');
+
+  /// File path: assets/svg/tool.svg
+  SvgGenImage get tool => const SvgGenImage('assets/svg/tool.svg');
+
+  /// File path: assets/svg/tool_2.svg
+  SvgGenImage get tool2 => const SvgGenImage('assets/svg/tool_2.svg');
+
+  /// File path: assets/svg/unit.svg
+  SvgGenImage get unit => const SvgGenImage('assets/svg/unit.svg');
+
+  /// File path: assets/svg/unit_2.svg
+  SvgGenImage get unit2 => const SvgGenImage('assets/svg/unit_2.svg');
+
+  /// File path: assets/svg/vehicle.svg
+  SvgGenImage get vehicle => const SvgGenImage('assets/svg/vehicle.svg');
+
+  /// File path: assets/svg/vehicle_2.svg
+  SvgGenImage get vehicle2 => const SvgGenImage('assets/svg/vehicle_2.svg');
+
+  /// File path: assets/svg/vehicle_3.svg
+  SvgGenImage get vehicle3 => const SvgGenImage('assets/svg/vehicle_3.svg');
+
+  /// File path: assets/svg/work.svg
+  SvgGenImage get work => const SvgGenImage('assets/svg/work.svg');
+
+  /// File path: assets/svg/work_2.svg
+  SvgGenImage get work2 => const SvgGenImage('assets/svg/work_2.svg');
+
+  /// File path: assets/svg/world.svg
+  SvgGenImage get world => const SvgGenImage('assets/svg/world.svg');
+
+  /// File path: assets/svg/world_2.svg
+  SvgGenImage get world2 => const SvgGenImage('assets/svg/world_2.svg');
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconGen icon = $AssetsIconGen();
+  static const $AssetsLottieGen lottie = $AssetsLottieGen();
+  static const $AssetsPrefectureIndonesiaGen prefectureIndonesia =
+      $AssetsPrefectureIndonesiaGen();
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
+
+class AssetGenImage extends AssetImage {
+  const AssetGenImage(String assetName) : super(assetName);
+
+  Image image({
+    Key? key,
+    ImageFrameBuilder? frameBuilder,
+    ImageLoadingBuilder? loadingBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? width,
+    double? height,
+    Color? color,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    FilterQuality filterQuality = FilterQuality.low,
+  }) {
+    return Image(
+      key: key,
+      image: this,
+      frameBuilder: frameBuilder,
+      loadingBuilder: loadingBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      width: width,
+      height: height,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      filterQuality: filterQuality,
+    );
+  }
+
+  String get path => assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    Color? color,
+    BlendMode colorBlendMode = BlendMode.srcIn,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    Clip clipBehavior = Clip.hardEdge,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      clipBehavior: clipBehavior,
+    );
+  }
+
+  String get path => _assetName;
+}
