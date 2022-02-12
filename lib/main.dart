3a2fa6584da760cac86e13ddad28d61f54791737
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2022-02-12 12:29:22 +0900

    画面縦固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:indonesia_flash_card/screen/lesson_selector_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
