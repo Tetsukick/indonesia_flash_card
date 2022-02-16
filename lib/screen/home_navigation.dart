44653857e684856cd6141628a0c7f1f325bf29b0
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2022-02-17 01:27:36 +0900

    add navigation bar

--- lib/screen/home_navigation.dart
+++ lib/screen/home_navigation.dart
@@ -1,15 +1,40 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:indonesia_flash_card/screen/lesson_selector_screen.dart';
+
+import '../config/color_config.dart';
 
 class HomeNavigation extends StatefulWidget {
-  const HomeNavigation({Key key}) : super(key: key);
+  const HomeNavigation({Key? key}) : super(key: key);
 
   @override
   _HomeNavigationState createState() => _HomeNavigationState();
 }
 
 class _HomeNavigationState extends State<HomeNavigation> {
+  int _page = 0;
+  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorConfig.bgPinkColor,
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        backgroundColor: ColorConfig.bgPinkColor,
+        items: <Widget>[
+          Icon(FontAwesomeIcons.book, size: 30),
+          Icon(FontAwesomeIcons.list, size: 30),
+          Icon(FontAwesomeIcons.bars, size: 30),
+        ],
+        onTap: (index) => setState(() => _page = index),
+      ),
+      body: pages(),
+    );
+  }
+
+  Widget pages() {
+    return LessonSelectorScreen();
   }
 }
