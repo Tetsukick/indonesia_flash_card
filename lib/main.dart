e54096e9831799e87b161e60f59eebba916c2bb1
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2022-02-25 23:57:05 +0900

    add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
@@ -6,20 +8,32 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:indonesia_flash_card/config/color_config.dart';
 import 'package:indonesia_flash_card/screen/home_navigation.dart';
 import 'package:indonesia_flash_card/screen/lesson_selector_screen.dart';
+import 'package:indonesia_flash_card/utils/crash_reporter.dart';
 import 'package:indonesia_flash_card/utils/utils.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-  runApp(
-    ProviderScope(
-      child: FlushCardApp(),
-    ),
-  );
+
+  runZonedGuarded(() async {
+    await CrashReporter.instance.initialize();
+
+    FlutterError.onError = (FlutterErrorDetails details) {
+      CrashReporter.instance.report(details.exceptionAsString(), details.stack);
+    };
+
+    runApp(
+      const ProviderScope(
+        child: FlushCardApp(),
+      ),
+    );
+  }, (error, stack) {
+    CrashReporter.instance.report(error, stack);
+  });
 }
 
 class FlushCardApp extends StatelessWidget {
