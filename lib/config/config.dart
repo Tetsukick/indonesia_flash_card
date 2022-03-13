b34aa08aa6da077c965c0b5ca6ca4e8b8d5fb29f
Teppei Kikuchi <>
2022-03-13 18:00:16 +0900

    create api base

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -7,4 +7,9 @@ class Config {
   static const adUnitIdIosBanner = kDebugMode ? 'ca-app-pub-3940256099942544/2934735716' : 'ca-app-pub-8604906384604870/3596398411';
   static const adUnitIdAndroidInterstitial = kDebugMode ? 'ca-app-pub-3940256099942544/8691691433' : 'ca-app-pub-8604906384604870/4045826689';
   static const adUnitIdIosInterstitial = kDebugMode ? 'ca-app-pub-3940256099942544/5135589807' : 'ca-app-pub-8604906384604870/9323926290';
+
+  static const apiKeyHeader = "x-api-key";
+  static const contentTypeHeader = "Content-type";
+  static const acceptHeader = "Accept";
+  static const authorizationHeader = "Authorization";
 }
