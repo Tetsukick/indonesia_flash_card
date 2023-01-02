cf8c42b2acbb0130c6c2882cba2871d16c424b3f
Teppei Kikuchi <>
2023-01-03 02:13:28 +0700

    画面外タップでキーボードを閉じる処理の追加

--- lib/screen/question/answer_list_screen.dart
+++ lib/screen/question/answer_list_screen.dart
@@ -85,196 +85,199 @@ class _QuestionAnswerListWidgetState extends State<QuestionAnswerListWidget> {
         elevation: 0,
       ),
       body: SafeArea(
-        child: Stack(
-          children: [
-            Stack(
-              children: [
-                SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 3,
-                              color: Color(0x39000000),
-                              offset: Offset(0, 1),
-                            )
-                          ],
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(),
-                              child: QuestionTitle(
-                                question: widget.questionEntity.question,
-                                maxLines: 5,
+        child: GestureDetector(
+          onTap: () => answerTextFieldFocusNode.unfocus(),
+          child: Stack(
+            children: [
+              Stack(
+                children: [
+                  SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 3,
+                                color: Color(0x39000000),
+                                offset: Offset(0, 1),
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(),
+                                child: QuestionTitle(
+                                  question: widget.questionEntity.question,
+                                  maxLines: 5,
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(16, 4, 16, 12),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  Expanded(
-                                    child: Text(
-                                      widget.questionEntity.createdAt.toString() ?? '',
-                                      textAlign: TextAlign.end,
-                                      style: FlutterFlowTheme.of(context)
+                              Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(16, 4, 16, 12),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        widget.questionEntity.createdAt.toString() ?? '',
+                                        textAlign: TextAlign.end,
+                                        style: FlutterFlowTheme.of(context)
+                                            .bodyText2
+                                            .override(
+                                          fontFamily: 'Lexend Deca',
+                                          color: Color(0xFF95A1AC),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ListView.builder(
+                          itemCount: questionAnswers.length,
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (BuildContext context, int index) {
+                            return AnswerCard(answerEntity: questionAnswers[index]);
+                          },
+                        ),
+                        const SizedBox(height: 120)
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: AlignmentDirectional(0, 1),
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Color(0x9AFFFFFF),
+                      ),
+                      child: Align(
+                        alignment: AlignmentDirectional(0, 1),
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding:
+                                  EdgeInsetsDirectional.fromSTEB(16, 12, 8, 0),
+                                  child: TextFormField(
+                                    controller: answerTextEditingController,
+                                    focusNode: answerTextFieldFocusNode,
+                                    decoration: InputDecoration(
+                                      labelText: '回答を送信',
+                                      labelStyle: FlutterFlowTheme.of(context)
                                           .bodyText2
                                           .override(
-                                        fontFamily: 'Lexend Deca',
-                                        color: Color(0xFF95A1AC),
+                                        fontFamily: 'Outfit',
+                                        color: Color(0xFF57636C),
                                         fontSize: 14,
                                         fontWeight: FontWeight.normal,
                                       ),
+                                      hintStyle: FlutterFlowTheme.of(context)
+                                          .bodyText2
+                                          .override(
+                                        fontFamily: 'Outfit',
+                                        color: Color(0xFF57636C),
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Color(0xFFE0E3E7),
+                                          width: 2,
+                                        ),
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Color(0xFFE0E3E7),
+                                          width: 2,
+                                        ),
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      errorBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Color(0x00000000),
+                                          width: 2,
+                                        ),
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      focusedErrorBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Color(0x00000000),
+                                          width: 2,
+                                        ),
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      filled: true,
+                                      fillColor: Color(0xFFF1F4F8),
+                                      contentPadding: EdgeInsetsDirectional.fromSTEB(
+                                          24, 24, 20, 24),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      ListView.builder(
-                        itemCount: questionAnswers.length,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (BuildContext context, int index) {
-                          return AnswerCard(answerEntity: questionAnswers[index]);
-                        },
-                      ),
-                      const SizedBox(height: 120)
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: AlignmentDirectional(0, 1),
-                  child: Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Color(0x9AFFFFFF),
-                    ),
-                    child: Align(
-                      alignment: AlignmentDirectional(0, 1),
-                      child: Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                EdgeInsetsDirectional.fromSTEB(16, 12, 8, 0),
-                                child: TextFormField(
-                                  controller: answerTextEditingController,
-                                  focusNode: answerTextFieldFocusNode,
-                                  decoration: InputDecoration(
-                                    labelText: '回答を送信',
-                                    labelStyle: FlutterFlowTheme.of(context)
-                                        .bodyText2
+                                    style: FlutterFlowTheme.of(context)
+                                        .bodyText1
                                         .override(
                                       fontFamily: 'Outfit',
-                                      color: Color(0xFF57636C),
+                                      color: Color(0xFF1D2429),
                                       fontSize: 14,
                                       fontWeight: FontWeight.normal,
                                     ),
-                                    hintStyle: FlutterFlowTheme.of(context)
-                                        .bodyText2
-                                        .override(
-                                      fontFamily: 'Outfit',
-                                      color: Color(0xFF57636C),
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Color(0xFFE0E3E7),
-                                        width: 2,
-                                      ),
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Color(0xFFE0E3E7),
-                                        width: 2,
-                                      ),
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    errorBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Color(0x00000000),
-                                        width: 2,
-                                      ),
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    focusedErrorBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Color(0x00000000),
-                                        width: 2,
-                                      ),
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    filled: true,
-                                    fillColor: Color(0xFFF1F4F8),
-                                    contentPadding: EdgeInsetsDirectional.fromSTEB(
-                                        24, 24, 20, 24),
+                                    maxLines: 6,
                                   ),
-                                  style: FlutterFlowTheme.of(context)
-                                      .bodyText1
-                                      .override(
-                                    fontFamily: 'Outfit',
-                                    color: Color(0xFF1D2429),
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                                  maxLines: 6,
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(0, 12, 12, 0),
-                              child: FlutterFlowIconButton(
-                                borderColor: Colors.transparent,
-                                borderRadius: 30,
-                                borderWidth: 1,
-                                buttonSize: 50,
-                                icon: Icon(
-                                  Icons.send,
-                                  color: Color(0xFFB71C1C),
-                                  size: 30,
+                              Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(0, 12, 12, 0),
+                                child: FlutterFlowIconButton(
+                                  borderColor: Colors.transparent,
+                                  borderRadius: 30,
+                                  borderWidth: 1,
+                                  buttonSize: 50,
+                                  icon: Icon(
+                                    Icons.send,
+                                    color: Color(0xFFB71C1C),
+                                    size: 30,
+                                  ),
+                                  onPressed: () {
+                                    answerTextFieldFocusNode.unfocus();
+                                    sendAnswer();
+                                  },
                                 ),
-                                onPressed: () {
-                                  answerTextFieldFocusNode.unfocus();
-                                  sendAnswer();
-                                },
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Visibility(
-              visible: _isSendingAnswer,
-              child: Container(
-                color: Colors.black.withOpacity(0.2),
-                child: Center(
-                  child: Lottie.asset(
-                    Assets.lottie.sendingPaperPlane,
-                    height: 300,
+                ],
+              ),
+              Visibility(
+                visible: _isSendingAnswer,
+                child: Container(
+                  color: Colors.black.withOpacity(0.2),
+                  child: Center(
+                    child: Lottie.asset(
+                      Assets.lottie.sendingPaperPlane,
+                      height: 300,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
