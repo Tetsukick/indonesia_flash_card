970362a879b17713b43ac0324f8bbea78c554008
Teppei Kikuchi <>
2023-01-08 17:17:09 +0700

    回答エリアのサイズ調整

--- lib/utils/common_border.dart
+++ lib/utils/common_border.dart
@@ -9,6 +9,14 @@ class CommonBorder {
     borderRadius: BorderRadius.circular(8),
   );
 
+  final grayOutlineInputBorder = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Color(0xFFE0E3E7),
+      width: 2,
+    ),
+    borderRadius: BorderRadius.circular(12),
+  );
+
   final transparentBorderSide = const BorderSide(
     color: Colors.transparent,
   );
