1c8964c8ef289b94268c7cd4f1ee15084b4b194c
Teppei Kikuchi <>
2023-12-12 16:31:24 +0700

    add tango database

---
+++ lib/model/floor_dao/tango_dao.dart
@@ -0,0 +1,21 @@
+import 'package:floor/floor.dart';
+import 'package:indonesia_flash_card/model/tango_entity.dart';
+
+import '../category.dart';
+import '../frequency.dart';
+import '../level.dart';
+import '../part_of_speech.dart';
+import '../sort_type.dart';
+import '../word_status_type.dart';
+
+@dao
+abstract class TangoDao {
+  @Query('SELECT * FROM TangoEntity ORDER BY indonesian ASC LIMIT :offset,:limit')
+  Future<List<TangoEntity>> getAllTangoList({int offset = 0, int limit = 100});
+
+  @update
+  Future<void> updateTangoEntity(TangoEntity tango);
+
+  @insert
+  Future<void> insertTangoEntity(TangoEntity tango);
+}
--- lib/model/floor_database/database.g.dart
+++ lib/model/floor_database/database.g.dart
@@ -65,13 +65,15 @@ class _$AppDatabase extends AppDatabase {
 
   ActivityDao? _activityDaoInstance;
 
+  TangoDao? _tangoDaoInstance;
+
   Future<sqflite.Database> open(
     String path,
     List<Migration> migrations, [
     Callback? callback,
   ]) async {
     final databaseOptions = sqflite.OpenDatabaseOptions(
-      version: 2,
+      version: 3,
       onConfigure: (database) async {
         await database.execute('PRAGMA foreign_keys = ON');
         await callback?.onConfigure?.call(database);
@@ -90,6 +92,8 @@ class _$AppDatabase extends AppDatabase {
             'CREATE TABLE IF NOT EXISTS `WordStatus` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `wordId` INTEGER NOT NULL, `status` INTEGER NOT NULL, `isBookmarked` INTEGER NOT NULL)');
         await database.execute(
             'CREATE TABLE IF NOT EXISTS `Activity` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `wordId` INTEGER NOT NULL, `date` TEXT NOT NULL)');
+        await database.execute(
+            'CREATE TABLE IF NOT EXISTS `TangoEntity` (`id` INTEGER, `indonesian` TEXT, `japanese` TEXT, `english` TEXT, `description` TEXT, `example` TEXT, `exampleJp` TEXT, `level` INTEGER, `partOfSpeech` INTEGER, `category` INTEGER, `frequency` INTEGER, `rankFrequency` INTEGER, PRIMARY KEY (`id`))');
 
         await callback?.onCreate?.call(database, version);
       },
@@ -105,6 +109,11 @@ class _$AppDatabase extends AppDatabase {
   @override
   ActivityDao get activityDao {
     return _activityDaoInstance ??= _$ActivityDao(database, changeListener);
+  }
+
+  @override
+  TangoDao get tangoDao {
+    return _tangoDaoInstance ??= _$TangoDao(database, changeListener);
   }
 }
 
@@ -241,5 +250,90 @@ class _$ActivityDao extends ActivityDao {
   @override
   Future<void> insertActivity(Activity activity) async {
     await _activityInsertionAdapter.insert(activity, OnConflictStrategy.abort);
+  }
+}
+
+class _$TangoDao extends TangoDao {
+  _$TangoDao(
+    this.database,
+    this.changeListener,
+  )   : _queryAdapter = QueryAdapter(database),
+        _tangoEntityInsertionAdapter = InsertionAdapter(
+            database,
+            'TangoEntity',
+            (TangoEntity item) => <String, Object?>{
+                  'id': item.id,
+                  'indonesian': item.indonesian,
+                  'japanese': item.japanese,
+                  'english': item.english,
+                  'description': item.description,
+                  'example': item.example,
+                  'exampleJp': item.exampleJp,
+                  'level': item.level,
+                  'partOfSpeech': item.partOfSpeech,
+                  'category': item.category,
+                  'frequency': item.frequency,
+                  'rankFrequency': item.rankFrequency
+                }),
+        _tangoEntityUpdateAdapter = UpdateAdapter(
+            database,
+            'TangoEntity',
+            ['id'],
+            (TangoEntity item) => <String, Object?>{
+                  'id': item.id,
+                  'indonesian': item.indonesian,
+                  'japanese': item.japanese,
+                  'english': item.english,
+                  'description': item.description,
+                  'example': item.example,
+                  'exampleJp': item.exampleJp,
+                  'level': item.level,
+                  'partOfSpeech': item.partOfSpeech,
+                  'category': item.category,
+                  'frequency': item.frequency,
+                  'rankFrequency': item.rankFrequency
+                });
+
+  final sqflite.DatabaseExecutor database;
+
+  final StreamController<String> changeListener;
+
+  final QueryAdapter _queryAdapter;
+
+  final InsertionAdapter<TangoEntity> _tangoEntityInsertionAdapter;
+
+  final UpdateAdapter<TangoEntity> _tangoEntityUpdateAdapter;
+
+  @override
+  Future<List<TangoEntity>> getAllTangoList(
+    int offset,
+    int limit,
+  ) async {
+    return _queryAdapter.queryList(
+        'SELECT * FROM TangoEntity ORDER BY indonesian ASC LIMIT ?1,?2',
+        mapper: (Map<String, Object?> row) => TangoEntity(
+            id: row['id'] as int?,
+            indonesian: row['indonesian'] as String?,
+            japanese: row['japanese'] as String?,
+            english: row['english'] as String?,
+            description: row['description'] as String?,
+            example: row['example'] as String?,
+            exampleJp: row['exampleJp'] as String?,
+            level: row['level'] as int?,
+            partOfSpeech: row['partOfSpeech'] as int?,
+            category: row['category'] as int?,
+            frequency: row['frequency'] as int?,
+            rankFrequency: row['rankFrequency'] as int?),
+        arguments: [offset, limit]);
+  }
+
+  @override
+  Future<void> insertTangoEntity(TangoEntity tango) async {
+    await _tangoEntityInsertionAdapter.insert(tango, OnConflictStrategy.abort);
+  }
+
+  @override
+  Future<void> updateTangoEntity(TangoEntity tango) async {
+    await _tangoEntityUpdateAdapter.update(tango, OnConflictStrategy.abort);
   }
 }
